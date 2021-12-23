717f05bdcc596410896d6754a62ef36e161f3de0
phoenixsnpd <phoenix.snpd@gmail.com>
2021-12-23 20:42:38 +0200

    Создаем популяцию

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        road();
-        int[] points = road();
+        road(); // создаем заданную стоимость пунктов пропуска.
+        int[] points = road(); // присваиваем в массив.
         System.out.println("Сегодняшняя стоимость на пунктах пропусков: ");
         for (int i = 0; i < points.length; i++) {
             System.out.print(points[i] + ", ");
         }
-        driver();
-        Integer[] driver1 =  driver();
-        for (int i = 0; i < driver1.length; i++) {
-            System.out.print(driver1[i]);
+        List<Integer[]> population = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            population.add(driver());
         }
     }
 
@@ -29,14 +28,14 @@ public class Main {
             counter += array[i];
             if (i == 9 && counter <= 55) {
                 road();
-        }
+            }
         }
         return array;
     }
 
     public static Integer[] driver() {
         List<Integer> newDriver = new ArrayList<>();
-        for (int i = 0; i < newDriver.size(); i++) {
+        for (int i = 1; i <= 10; i++) {
             newDriver.add(i);
         }
         Collections.shuffle(newDriver);
